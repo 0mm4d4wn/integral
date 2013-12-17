688b3aa34f4897f3bdb424b32201f9d7fb749a94
0mm4d4wn <fbuitragosanta@gmail.com>
2013-12-17 17:00:04 -0500

    Drupal & tema - primer intento

--- site/sites/all/themes/zeropoint/templates/page.tpl.php
+++ site/sites/all/themes/zeropoint/templates/page.tpl.php
@@ -8,6 +8,7 @@
 			</div><!-- logo -->
 			<div class="col-md-9">
 				<div class="btn-group language pull-right">
+            <div id="user_links"><?php print zeropoint_login() ?></div>
 	  			<button class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
 					Idiomas
 					<span class="caret"></span>
@@ -26,7 +27,27 @@
 	</div>
 	<div class="container-fluid menu">
 		<div class="container">
-			<nav id="navigation" class="texto-nav pull-right">
+
+      <?php if ($main_menu || $page['dropdown']): ?>
+          <div id="navlinks" class="<?php print menupos() ?> pull-right">
+          <?php if (!empty($page['dropdown'])) { ?><?php print render($page['dropdown']); ?><?php } ?>
+          <?php if (empty($page['dropdown'])) { ?>
+          <?php print theme('links__system_main_menu', array(
+                    'links' => $main_menu,
+                    'attributes' => array(
+                      'id' => 'navlist',
+                      'class' => array('links', 'clearfix'),
+                    ),
+                    'heading' => array(
+                      'text' => t('Main menu'),
+                      'level' => 'h2',
+                      'class' => array('element-invisible'),
+                    ),)); ?><?php } ?>
+          </div>
+        <?php endif;  ?>
+
+		<!--
+      <nav id="navigation" class="texto-nav pull-right">
 				<ul class="list-inline nav nav-pills">
 					<li>
 						<a class="active" href="#">
@@ -55,143 +76,21 @@
 					</li>
 				</ul>
 			</nav>
+    -->
+
 		</div>
 	</div>
 </header>
-<div class="container-fluid">
-	<div class="slider">
 
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-		  <!-- Indicators -->
-		  <ol class="carousel-indicators">
-		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		  </ol>
-
-		  <!-- Wrapper for slides -->
-		  <div class="carousel-inner">
-		    <div class="item active">
-		      <img src="vitoslider1.png" alt="...">
-		      <div class="carousel-caption">
-		        <h2>First slide label</h2>
-		        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-		      </div>
-		    </div>
-		  </div>
-
-		  <!-- Controls -->
-		  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-		    <span class="glyphicon glyphicon-chevron-left"></span>
-		  </a>
-		  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-		    <span class="glyphicon glyphicon-chevron-right"></span>
-		  </a>
-		</div>
-
-	</div><!-- slider -->
-
-	<div class="container content">
-		<div class="col-md-3 sidebar">
-			sidebar
-			<!-- Nav tabs -->
-			<ul class="nav nav-tabs">
-			  <li><a href="#" data-toggle="tab">Home</a></li>
-			  <li><a href="#" data-toggle="tab">Profile</a></li>
-			</ul>
-
-			<!-- Tab panes -->
-			<div class="tab-content">
-			  <div class="tab-pane active" id="home">...</div>
-			  <div class="tab-pane" id="profile">...</div>
-			  <div class="tab-pane" id="messages">...</div>
-			  <div class="tab-pane" id="settings">...</div>
-			</div>
-
-		</div><!--sidebar-->
-
-		<div class="contenedor">
-		<a href="#">
-			<div class="col-md-3 propiedades box property-1">
-					<div class="bg-title-box"></div>
-					<div class="piedefoto title-box">Propiedad 1</div>
-					<img src="vito-property.jpg" alt="...">
-				<div class="piedefoto title-box-bottom">Característica de propiedad 1</div>
-			</div>
-		</a>
-		<a href="#">
-			<div class="col-md-3 propiedades box">
-				<div class="">
-					<div class="piedefoto title-box">Propiedad 2</div>
-				</div>
-				<img src="vito-property2.jpg" alt="...">
-			</div>
-		</a>
-		<a href="#">
-			<div class="col-md-3 propiedades box">
-				<div class="">
-					<div class="piedefoto title-box">Propiedad 3</div>
-				</div>
-				<img src="vito-property3.jpg" alt="...">
-			</div>
-		</a>
-		<a href="#">
-			<div class="col-md-3 propiedades box">
-				<div class="">
-					<div class="piedefoto title-box">Propiedad 4</div>
-				</div>
-				<img src="vito-property4.jpg" alt="...">
-			</div>
-		</a>
-		<a href="#">
-			<div class="col-md-3 propiedades box">
-				<div class="">
-					<div class="piedefoto title-box">Propiedad 5</div>
-				</div>
-				<img src="vito-property5.jpg" alt="...">
-			</div>
-		</a>
-	</div>
-	</div>
-	<div class="container content">
-		<div class="col-md-12 box espacio-opcional">
-			espacio para lo que sea
-		</div>
-		<div class="fivecolumns">
-			<div class="col-sm-2 box contactos-paises">
-				contacto panama
-			</div>
-			<div class="col-sm-2 box contactos-paises">
-				contacto venezuela
-			</div>
-			<div class="col-sm-2 box contactos-paises">
-				contacto colombia
-			</div>
-			<div class="col-sm-2 box contactos-paises">
-				contacto estados unidos
-			</div>
-			<div class="col-sm-2 box contactos-paises">
-				contacto union europea
-			</div>
-		</div>
-	</div>
+<div id="main-container">
+  <?php if ($page['highlighted']): ?><div id="mission"><?php print render ($page['highlighted']); ?></div><?php endif; ?>
+  <?php print render($title_prefix); ?>
+  <?php if ($title): if ($is_front){ print '<h2 class="title">'. $title .'</h2>'; } else { print '<h1 class="title">'. $title .'</h1>'; } endif; ?>
+  <?php print render($title_suffix); ?>
+  <div class="tabs"><?php print render($tabs); ?></div>
+  <?php print render($page['help']); ?>
+  <?php print $messages ?>
+  <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+  <?php if ($page['content']) : ?><?php print render ($page['content']); ?><?php endif; ?>
+  <?php print $feed_icons; ?>
 </div>
-<div class="container-fluid footer">
-	<div class="container content">
-		footer
-		<div class="fivecolumns">
-			<div class="col-sm-2 box contactos-paises">
-				redes sociales
-			</div>
-			<div class="col-sm-5 box contactos-paises">
-				datos de copyright, contacto y eso.
-			</div>
-			<div class="col-sm-2 box contactos-paises">
-				nota legal
-			</div>
-		</div>
-	</div>
-</div>
-
-
-
