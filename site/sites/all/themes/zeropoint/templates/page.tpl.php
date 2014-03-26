70c7e40640be60d3545185fab330c8263bacd8fc
jose david arias escobar <arcoles.d@gmail.com>
2014-03-25 21:09:13 -0500

    [CSS] estilos header + titles

--- site/sites/all/themes/zeropoint/templates/page.tpl.php
+++ site/sites/all/themes/zeropoint/templates/page.tpl.php
@@ -5,7 +5,7 @@
 <!-- ######################## Main Menu ######################## -->
 <nav>
   <div class="twelve columns header_nav">
-    <div class="row">
+    <div class="row header">
       <a href="#" id="brand" class=""></a>
       <?php if ($main_menu || $page['dropdown']): ?>
       <div id="menu" class="<?php //print menupos() ?>">
@@ -33,44 +33,6 @@
   </div>    
 </nav>
 <!-- ######################## Header (featured posts) ######################## -->
-<!--   
-<header>
-  <div class="row">
-    <h1>Most Read The Last 24 hrs.</h1>
-    <article class="four columns featured_post">
-      <h3>Lorem Ipsum</h3> 
-      <img src="images/demo2_small.jpg" alt="desc" style="width:100%" />
-      <p> Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
-      <div class="post_meta">
-        <span class="lsf-icon" title="calender">20th oct 2012</span> 
-        <span class="lsf-icon" title="user" style="margin-left:15px"><a href="#">Admin</a></span>
-        <span class="lsf-icon" title="comment" style="margin-left:15px"><a href="#">5</a></span>
-      </div> 
-    </article>
-    <article class="four columns featured_post">
-      <h3>Mauris Phatera</h3> 
-      <img src="images/demo3_small.jpg" alt="desc" style="width:100%" />
-      <p> Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
-      <div class="post_meta">
-        <span class="lsf-icon" title="calender">20th oct 2012</span> 
-        <span class="lsf-icon" title="user" style="margin-left:15px"><a href="#">Admin</a></span>
-        <span class="lsf-icon" title="comment" style="margin-left:15px"><a href="#">5</a></span>
-      </div> 
-    </article>
-    <article class="four columns featured_post">
-      <h3>Dolores et Mollis</h3> 
-      <img src="images/demo1_small.jpg" alt="desc" style="width:100%" />
-      <p> Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
-      <div class="post_meta">
-        <span class="lsf-icon" title="calender">20th oct 2012</span> 
-        <span class="lsf-icon" title="user" style="margin-left:15px"><a href="#">Admin</a></span>
-        <span class="lsf-icon" title="comment" style="margin-left:15px"><a href="#">5</a></span>
-      </div> 
-    </article>
-  </div>    
-</header>
--->
-<!-- ######################## Section ######################## -->
 <section>
   <div class="section_main">
     <div class="row">
@@ -96,48 +58,6 @@
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php if ($page['content']) : ?><?php print render ($page['content']); ?><?php endif; ?>
         <?php print $feed_icons; ?>
-<!--
-<h2>Latest</h2>
-<article class="blog_post">
-  <div class="three columns">
-    <a href="#" class="th">
-      <img src="images/thumb1.jpg" alt="desc" />
-    </a>
-  </div>
-  <div class="nine columns">
-    <a href="#">
-      <h3>Lorem Ipsum</h3>
-    </a>
-    <p> Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
-  </div>
-</article>
-<article class="blog_post">
-  <div class="three columns">
-    <a href="#" class="th">
-      <img src="images/thumb2.jpg" alt="desc" />
-    </a>
-  </div>
-  <div class="nine columns">
-    <a href="#">
-      <h3>Lorem Ipsum</h3>
-    </a>
-    <p> Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
-  </div>
-</article>
-<article class="blog_post">
-  <div class="three columns">
-    <a href="#" class="th">
-      <img src="images/pin2.jpg" alt="desc" />
-    </a>
-  </div>
-  <div class="nine columns">
-    <a href="#">
-      <h3>Lorem Ipsum</h3>
-    </a>
-    <p> Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus. Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus.</p>
-  </div>
-</article>
--->  
       </section>  
     </div>
   </div>
@@ -215,34 +135,3 @@ cel: (507) 6612-7847</p>
     <!-- Initialize JS Plugins -->
      <script src="javascripts/app.js" type="text/javascript"></script>
 </div>
-<!--
-  <h3>heading</h3>
-  <p> Vivamus tortor tellus, rutrum sit amet mollis vel, imperdiet consectetur orci. Mauris pharetra congue enim, et sagittis lectus congue ut. Cum sociis natoque penatibus. Vivamus tortor tellus, rutrum sit amet mollis vel.</p>
-  <h3>Accordion Panel</h3>
-  <ul class="accordion">
-    <li class="active">
-      <div class="title">
-        <h5>Accordion Panel 1</h5>
-      </div>
-      <div class="content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      </div>
-    </li>
-    <li>
-      <div class="title">
-        <h5>Accordion Panel 2</h5>
-      </div>
-      <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div>
-    </li>
-    <li>
-      <div class="title">
-        <h5>Accordion Panel 3</h5>
-      </div>
-      <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
-      </div>
-    </li>
-  </ul>
--->
